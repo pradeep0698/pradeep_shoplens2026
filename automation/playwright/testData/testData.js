e8bdf4bf59877b565a69fe99dbddc2966ba8b357
shoplensai-coder <shoplens.ai@gmail.com>
2026-06-26 14:52:07 -0500

    Update testData.js

--- automation/playwright/testData/testData.js
+++ automation/playwright/testData/testData.js
@@ -2,17 +2,13 @@ const path = require('path');
 
 const credentials = {
   regularUser: {
-    userId: process.env.SHOPLENS_REGULAR_USER || 'eprabha2019@gmail.com',
-    password: process.env.SHOPLENS_REGULAR_PASSWORD || ['Cook', 'shop123!'].join(''),
+    userId: 'your-email',
+    password: 'your-password',
   },
 };
 
 const files = {
-  imageFileName: 'Nikeshoe.png',
-  imagePath: path.join(__dirname, 'Nikeshoe.png'),
+  imagePath: path.join(process.cwd(), 'test-data', 'images', 'Nikeshoe.png'),
 };
 
-module.exports = {
-  credentials,
-  files,
-};
+module.exports = { credentials, files };

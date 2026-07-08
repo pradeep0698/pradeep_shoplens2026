820c6dd92bd3f8b2af8175ac6e26ebd44bd68f57
Bijal Mugatwala <33298527+bijalm@users.noreply.github.com>
2026-07-07 19:21:38 -0700

    Update about_screen.dart

--- mobile/lib/presentation/screens/about_screen.dart
+++ mobile/lib/presentation/screens/about_screen.dart
@@ -28,7 +28,7 @@ class AboutScreen extends StatelessWidget {
             const SizedBox(height: 24),
             const SizedBox(height: 8),
             const Text(
-              'Version 1.32.2',
+              'Version 1.32.4',
               style: TextStyle(color: Color(0xFF94A3B8), fontSize: 15),
             ),
           ],
